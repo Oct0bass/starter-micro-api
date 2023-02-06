fea7b95c64ff364bca7bd85780bf38aed3b70f1e
Rohan Rajagopal <69211265+Oct0bass@users.noreply.github.com>
2023-02-05 18:16:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    if (req.path == "env") {
+        res.write(JSON.stringify(process.env));
+    } else if (req.path.startsWith("env/")) {
+        res.write(process.env[req.path.slice(3));
+    } else {
+        res.write('Yo!');
+    }
     res.end();
 }).listen(process.env.PORT || 3000);
