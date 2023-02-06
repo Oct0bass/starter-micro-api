3996741a8942d0ae741d5f0745538e03bfa3c13f
Rohan Rajagopal <69211265+Oct0bass@users.noreply.github.com>
2023-02-05 18:23:33 -0700

    Fix again

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    if (req.path == "env") {
+    const reqData = req.read()
+    if (reqData == "env") {
         res.write(JSON.stringify(process.env));
-    } else if (req.path.startsWith("env/")) {
+    } else if (reqData.startsWith("env/")) {
         res.write(process.env[req.path.slice(3)]);
     } else {
         res.write('Yo!');
